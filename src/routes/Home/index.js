0701c1f4fb882e1c903fec7aec024f7c4e76f471
何方 <2018995960@qq.com>
2018-08-23 14:49:30 +0800

    测试 lint-staged

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,10 +1,12 @@
 import React, { PureComponent } from 'react';
 import styles from './Home.less';
 
+const hh='nuihoa';
 export default class Home extends PureComponent {
+
   render() {
     return (
-      <div id="home" className={styles.home}>这是home</div>
+      <div id="home" className={styles.home}>这是home{hh}</div>
     )
   }
 }

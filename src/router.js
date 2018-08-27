6a280a82f1281b961c0a1bcda0fe3a762bf63d69
何方 <2018995960@qq.com>
2018-08-27 14:22:57 +0800

    1

--- src/router.js
+++ src/router.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import Index from "./routes/Index";
 
 function RouterConfig({ history }) {
-  return (
-    <Router history={history}>
+  return <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/" exact component={Index} />
       </Switch>
-    </Router>
-  );
+    </Router>;
 }
 
 export default RouterConfig;
---
+++ src/routes/Index/index.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import { Button } from 'antd-mobile';
+
+export default class Index extends PureComponent {
+  render() {
+    return(
+      <div>
+        <Button>点击</Button>
+      </div>
+    )
+  }
+}

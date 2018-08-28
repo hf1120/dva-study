f3ef65765cd98f1abcb6f1d60e03348a7aaac97d
何方 <2018995960@qq.com>
2018-08-28 14:23:18 +0800

    1

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 
 export default class IndexPage extends PureComponent {
   componentDidMount() {
-    const a = 111;
+    const a = 1121;
     if (a) {
       console.log(a);
     }

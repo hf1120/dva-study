5185a10d7258580801f2addbc007a14e0632781b
何方 <2018995960@qq.com>
2018-08-28 13:49:55 +0800

    1

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import { connect } from 'dva';
-import styles from './IndexPage.css';
+import React, { PureComponent } from 'react';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
+export default class IndexPage extends PureComponent {
+  componentDidMount() {
+    const a = 1;
+    if (a) {
+      console.log(a);
+    }
+  }
+
+  render() {
+    return (
+      <div>1</div>
+    );
+  }
 }
-
-IndexPage.propTypes = {
-};
-
-export default connect()(IndexPage);
